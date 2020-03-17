aca161ccdde86fb14a117a0b2c5b8f0cb6cdd632
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-17 15:37:08 +0300

    feat: complete 10 % of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,83 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let arr = expr.split(' ');
+  let stack1 = [];
+  let stack2 = [];
+  let stack3 = [];
+  let obj = {
+      "+": 1,
+      "-": 1,
+      "*": 2,
+      "/": 2,
+  }
+
+  let plus = "+";
+  let minus = "-";
+  let multi = "*";
+  let div = "/"
+
+  for (let i = 0; i < arr.length; i++) {
+      if (isNaN(Number(arr[i])) == false && arr[i] !== "") {
+          stack1.push(+arr[i]);
+      }
+      else if (isNaN(Number(arr[i])) && stack2.length == 0) {
+        stack2.push((arr[i]));
+      } 
+      else if (isNaN(Number(arr[i])) && obj[arr[i]] <= obj[stack2[(stack2.length-1)]]) {
+          if (stack2[stack2.length - 1] == "+") {
+         let add = stack1[(stack1.length - 2)] + stack1[(stack1.length - 1)]
+        stack1.pop();
+        stack1.pop();
+        stack1.push(add);
+        stack2.pop();
+        stack2.push(arr[i])
+          }
+        
+         else if (stack2[stack2.length - 1] == "-") {
+          let add = stack1[(stack1.length - 2)] - stack1[(stack1.length - 1)]
+          stack1.pop();
+          stack1.pop();
+          stack1.push(add);
+          stack2.pop();
+          stack2.push(arr[i]);
+            }
+            else if (stack2[stack2.length - 1] == "*") {
+              let add = stack1[(stack1.length - 2)] * stack1[(stack1.length - 1)]
+              stack1.pop();
+              stack1.pop();
+              stack1.push(add);
+              stack2.pop();
+              stack2.push(arr[i]);
+                }
+                else if (stack2[stack2.length - 1] == "/") {
+                  let add = stack1[(stack1.length - 2)] / stack1[(stack1.length - 1)]
+                  stack1.pop();
+                  stack1.pop();
+                  stack1.push(add);
+                  stack2.pop();
+                  stack2.push(arr[i]);
+                    }
+
+      else if (isNaN(Number(arr[i])) && (obj[arr[i]] > obj[stack2[(stack2.length-1)]])) {
+      stack2.push(arr[i])
+      }
+  }
+  }
+if (stack2[0] == "*") {
+    return (stack1[0] * stack1[1]);
+}
+else if (stack2[0] == "/") {
+    return (stack1[0] / stack1[1]);
+}
+else if (stack2[0] == "+") {
+    return (stack1[0] + stack1[1]);
+}
+else if (stack2[0] == "-") {
+    return (stack1[0] - stack1[1]);
 }
 
+
+}
 module.exports = {
     expressionCalculator
 }
