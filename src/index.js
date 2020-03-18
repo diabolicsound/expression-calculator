3fe2da4fd5214deaa41d59362b283c3d34a25758
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-18 16:39:28 +0300

    feat:few more tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 
 function expressionCalculator(expr) {
+    if (expr.includes('/ 0')) {
+        throw Error('TypeError: Division by zero.');
+    }
+    let openbrack = 0;
+    let closebrack = 0;
+
+    for (let z = 0; z < expr.length; z++) {
+        if (expr[z] == '('){
+            openbrack += 1;
+        }
+        else if (expr[z] == ')') {
+            closebrack += 1;
+        }
+    }
+   if (closebrack !== openbrack) {
+       throw Error('ExpressionError: Brackets must be paired')
+   }
     let arr = expr.split(' ');
   let stack1 = [];
   let stack2 = [];
@@ -10,7 +27,6 @@ function expressionCalculator(expr) {
       "*": 2,
       "/": 2,
   }
-
   for (let i = 0; i < arr.length; i++) {
       if (isNaN(+arr[i]) == false && arr[i] !== "") {
           stack1.push(+arr[i]);
@@ -19,7 +35,7 @@ function expressionCalculator(expr) {
           stack2.push(arr[i]);
       }
      
-      else if (isNaN(+arr[i]) && obj[arr[i]] <= obj[stack2[stack2.length - 1]]){
+      else if (isNaN(+arr[i]) && obj[arr[i]] <= obj[stack2[stack2.length - 1]] && obj[stack2[stack2.length - 1]] !== "("){
           if (stack2[stack2.length - 1] == "+") {
               let add = stack1[stack1.length -2] + stack1[stack1.length -1];
               stack1.pop();
@@ -27,6 +43,7 @@ function expressionCalculator(expr) {
               stack1.push(add);
               stack2.pop();
               stack2.push(arr[i]);
+
           }
          else if (stack2[stack2.length - 1] == "-") {
             let sub = stack1[stack1.length -2] - stack1[stack1.length -1];
@@ -35,6 +52,8 @@ function expressionCalculator(expr) {
             stack1.push(sub);
             stack2.pop();
             stack2.push(arr[i]);
+
+
         }
         else if (stack2[stack2.length - 1] == "*") {
             let mul = stack1[stack1.length -2] * stack1[stack1.length -1];
@@ -53,11 +72,10 @@ function expressionCalculator(expr) {
             stack2.push(arr[i]);
         }
       }
-      else if (isNaN(+arr[i]) && obj[arr[i]] > obj[stack2[stack2.length - 1]]) {
+      else if (isNaN(+arr[i]) && arr[i] !== ")") {
         stack2.push(arr[i]);
       }
-  }
-
+    } 
 for (let j = 0; j < 5; j++){
     if (stack2[stack2.length - 1] == "+") {
         let sum = stack1[stack1.length - 2] + stack1[stack1.length - 1];
@@ -66,13 +84,14 @@ for (let j = 0; j < 5; j++){
         stack1.push(sum);
         stack2.pop();
     }
+    
     else if (stack2[stack2.length - 1] == "-") {
         let mini = stack1[stack1.length - 2] - stack1[stack1.length - 1];
         stack1.pop();
         stack1.pop();
         stack1.push(mini);
        stack2.pop();
-    }
+    } 
     else if (stack2[stack2.length - 1] == "*") {
         let multic = stack1[stack1.length - 2] * stack1[stack1.length - 1];
         stack1.pop();
@@ -89,10 +108,13 @@ for (let j = 0; j < 5; j++){
     
     }
     
-}
-return stack1[0];
+} 
+console.log(stack1)
+console.log(stack2)
 
+return stack1[0];
 }
+
 module.exports = {
     expressionCalculator
 }
