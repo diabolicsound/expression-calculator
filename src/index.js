e9cd3a74ab52d294f928f167fd6e5fec3b8603e9
Artsemi Shareika <artsemishareika@gmail.com>
2020-03-18 19:14:42 +0300

    feat: complete more tests

--- src/index.js
+++ src/index.js
@@ -28,9 +28,16 @@ function expressionCalculator(expr) {
       "/": 2,
   }
   for (let i = 0; i < arr.length; i++) {
-      if (isNaN(+arr[i]) == false && arr[i] !== "") {
+    if (isNaN(arr[i]) && stack2[stack2.length - 1] == "-" && arr[i] !== "+") {
+        stack1[stack1.length - 1] = -(stack1[stack1.length - 1]);
+        stack2[stack2.length - 1] = "+"
+        stack2.push(arr[i]);
+    }
+     else if (isNaN(+arr[i]) == false && arr[i] !== "") {
           stack1.push(+arr[i]);
       }
+     
+    
       else if (isNaN(+arr[i]) && stack2.length == 0){
           stack2.push(arr[i]);
       }
@@ -112,9 +119,31 @@ function expressionCalculator(expr) {
         }
       }
     } 
+    for (let j = 0; j < 5; j++){
+     if (stack2[stack2.length - 1] == "-") {
+        let mini = stack1[stack1.length - 2] - stack1[stack1.length - 1];
+        stack1.pop();
+        stack1.pop();
+        stack1.push(mini);
+       stack2.pop();
+    } 
+    /*if (stack2[stack2.length - 1] == "-") {
+        let mini = stack1[stack1.length - 2] - stack1[stack1.length - 1];
+        stack1.pop();
+        stack1.pop();
+        stack1.push(mini);
+       stack2.pop();
+    } */
     
 for (let j = 0; j < 5; j++){
-    if (stack2[stack2.length - 1] == "+") {
+     if (stack2[stack2.length - 1] == "-") {
+        let mini = stack1[stack1.length - 2] - stack1[stack1.length - 1];
+        stack1.pop();
+        stack1.pop();
+        stack1.push(mini);
+       stack2.pop();
+    } 
+   else if (stack2[stack2.length - 1] == "+") {
         let sum = stack1[stack1.length - 2] + stack1[stack1.length - 1];
         stack1.pop();
         stack1.pop();
@@ -122,13 +151,6 @@ for (let j = 0; j < 5; j++){
         stack2.pop();
     }
     
-    else if (stack2[stack2.length - 1] == "-") {
-        let mini = stack1[stack1.length - 2] - stack1[stack1.length - 1];
-        stack1.pop();
-        stack1.pop();
-        stack1.push(mini);
-       stack2.pop();
-    } 
     else if (stack2[stack2.length - 1] == "*") {
         let multic = stack1[stack1.length - 2] * stack1[stack1.length - 1];
         stack1.pop();
@@ -143,15 +165,16 @@ for (let j = 0; j < 5; j++){
         stack1.push(div2);
        stack2.pop();
     
-    }
+    } 
     
-} 
+} /*
 console.log(stack1)
 console.log(stack2)
-
+console.log(-(stack1[1]))
+*/
 return stack1[0];
 }
-
+}
 module.exports = {
     expressionCalculator
 }
